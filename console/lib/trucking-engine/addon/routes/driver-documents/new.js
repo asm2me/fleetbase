fbefa691564fb717a2cd2452d977b97c7bbd56c8
asm2me <49681638+asm2me@users.noreply.github.com>
2026-04-30 13:06:17 +0300

    12

--- console/lib/trucking-engine/addon/routes/driver-documents/new.js
+++ console/lib/trucking-engine/addon/routes/driver-documents/new.js
@@ -2,5 +2,5 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 export default class DriverDocumentsNewRoute extends Route {
     @service store;
-    model() { return this.store.createRecord('trucking-driver-document', { status: 'active' }); }
+    model() { return this.store.createRecord('trucking-driver-document', { status: 'active', cdl_endorsements: [] }); }
 }
